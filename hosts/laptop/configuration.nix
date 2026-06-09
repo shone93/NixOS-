814aa77c2c8243ea93518c857084056cf533e284
shone93 <nenadcvijanovic93@gmail.com>
2026-06-09 22:40:07 +0200

    Dodat power management

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -102,7 +102,7 @@
   # Diskovi
   # ─────────────────────────────────────────────
   fileSystems."/mnt/games" = {
-    device = "/dev/sdb1";
+    device = "/dev/disk/by-uuid/e0c6d58c-8316-47fc-83c9-3e767ede2c50";
     fsType = "ext4";
     options = [
       "defaults"
