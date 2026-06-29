a38165b6c715dce5a08f2c7cd3b06f70d8c2fdc1
shone93 <nenadcvijanovic93@gmail.com>
2026-06-29 20:49:55 +0200

    rebuild

--- modules/system/apps-desktop.nix
+++ modules/system/apps-desktop.nix
@@ -3,11 +3,13 @@
 {
   # ─────────────────────────────────────────────
   # Teške aplikacije - SAMO desktop (SolidSnake)
-  # Laptop bi se mučio sa ovima
   # ─────────────────────────────────────────────
   environment.systemPackages = with pkgs; [
     blender # 3D modelovanje i animacija
     gimp # Editovanje slika (Photoshop alternativa)
     inkscape # Vektorska grafika (Illustrator alternativa)
+    krita
+    godot
+
   ];
 }
